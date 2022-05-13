91e77702bdbfbe36b8dbdb4be5c060e5e6794ed3
Shivam Pandey <sap0442@gmail.com>
2022-05-13 16:13:37 +0530

    implemented the hashing part and authenticate middleware

--- src/Routes/auth-login/auth-login.ts
+++ src/Routes/auth-login/auth-login.ts
@@ -1,14 +1,43 @@
-const {Router} =  require("express");
+require("dotenv").config();
+const { Router } = require("express");
 const session = require("express-session");
-// const app = express();
+const bcrypt = require("bcrypt");
 const loginauth = Router();
+const jwt = require("jsonwebtoken");
 
-const authenicate = (req: { session: any; },res: any,next: any) => {
-  console.log(req.session)
-  next()
+
+
+const {SESS_NAME = "sid",JWT_ACCESS_KEY="shivam"} = process.env
+
+const Users = [
+  {id:1,name:"shivam",email:"shivam@gmail.com",password:'password'},{id:2,name:"shiva",email:"shiva@gmail.com",password:'password'},{id:3,name:"pandey",email:"pandey@gmail.com",password:'password'},{id:4,name:"aditya",email:"aditya@gmail.com",password:'password'}
+]
+
+const newToken = (user) => {
+    return jwt.sign({ user },JWT_ACCESS_KEY)
 }
 
-loginauth.get("",authenicate,(req: any,res: { send: (arg0: string) => any; }) => {
+const authenicatelogin = (req: { session: any; },res: any,next: any) => {
+  console.log(req.session)
+  if (!req.session.token) {
+    res.redirect("/login");
+  }
+  else { 
+    next()
+  }
+}
+
+const authenicatehome = (req: { session: any; },res: any,next: any) => {
+  console.log(req.session)
+  if (req.session.token) {
+    res.redirect("/home");
+  }
+  else { 
+    next()
+  }
+}
+
+loginauth.get("",(req: any,res: { send: (arg0: string) => any; }) => {
     const {token} = req.session;
     return res.send(`
     <h1>Welcome to heed!</h1>
@@ -23,7 +52,7 @@ loginauth.get("",authenicate,(req: any,res: { send: (arg0: string) => any; }) =>
     `)
 })
 
-loginauth.get("/home",(req: any,res: { send: (arg0: string) => any; }) => {
+loginauth.get("/home",authenicatelogin,(req: any,res: { send: (arg0: string) => any; }) => {
 
   return res.send(`
     <h1>Welcome to Home Page</h1>
@@ -31,41 +60,54 @@ loginauth.get("/home",(req: any,res: { send: (arg0: string) => any; }) => {
 
 })
 
-loginauth.get("/login",(req: any,res: any) => {
+loginauth.get("/login",authenicatehome,(req: any,res: any) => {
    return res.send(`
      <h1>Login</h1>
      <form method="post" action="/login">
        <input type="email" name="email" placeholder="Enter your mail"/>
        <input type="password" name="password" placeholder="Enter your Password" />
-       <button>Login</button>
+       <input type="submit" value="Login">
       </form>
+      <a href='/register'>Register</a>
    `)
 })
 
-loginauth.get("/register",() => {
-
+loginauth.get("/register", authenicatehome, (req: any, res: { send: (arg0: string) => any; }) => {
+  return res.send(`
+  <h1>REgister</h1>
+  <form method="post" action="/register">
+    <input name="name" placeholder="Enter your Name" />
+    <input type="email" name="email" placeholder="Enter your mail"/>
+    <input type="password" name="password" placeholder="Enter your Password" />
+    <input type="submit" value="Register">
+   </form>
+   <a href='/login'>Login</a>
+  `)
 })
 
-loginauth.post("/login",(req: { body: any; },res: any) => {
+loginauth.post("/login", authenicatehome, async (req: { body: any; session: any; },res: any) => {
 
   try {
+    const { email, password } = req.body;
     // check the format of email first if not proper throw an error 
 
     // else check wheather email is present
-    const user = await User.find({ email: req.body.email });
+    const user = Users.find(user => user.email==email && user.password == password );
 
     // if not present throw an error 
     if (!user) return res.status(400).json({ status: "failed", message: "Provide a valid email address" })
 
-    // else verify the password
-    const validPassword = await bcrypt.compare(req.body.password, user.password);
-    if (validPassword) {
-      res.status(200).json({ message: "Valid password" });
-    } 
-    // if password not match throw an error
-    else {
-      res.status(400).json({ error: "Invalid Password" });
-    }
+    // comment out when linked to DB 
+
+    // // else verify the password
+    // const validPassword = await bcrypt.compare(password, user.password);
+    // if (validPassword) {
+    //   res.status(200).json({ message: "Successfully login" });
+    // } 
+    // // if password not match throw an error
+    // else {
+    //   res.status(400).json({ error: "Invalid Password" });
+    // }
     // else create a new token 
     const token = newToken(user);
 
@@ -81,12 +123,52 @@ loginauth.post("/login",(req: { body: any; },res: any) => {
 
 })
 
-loginauth.post("/register",() => {
+loginauth.post("/register", authenicatehome, async (req: any, res: any) => {
+  
+  const { name, email, password } = req.body;
+
+  if (name && email && password) {
+    const exists = Users.some(
+      user => user.email == email
+    )
+
+    if (!exists) {
+      const user = {
+        id: Users.length + 1,
+        name,
+        email,
+        password
+      }
+      //uncommented when link to DB
+
+      // const salt = await bcrypt.genSalt(10);
+      // // now we set user password to hashed password
+      // user.password = await bcrypt.hash(user.password, salt);
+    
+
+      //  await Users.push(user);
+
+        // create the token
+
+      const token = newToken(user);
+      req.session.token = token;
+      return res.redirect("/home")
+    }
+    
+  }
+
+  return res.redirect("/register") //todo error message
     
 })
 
-loginauth.post("/logout",() => {
-
+loginauth.post("/logout", authenicatelogin, (req: any, res: any) => {
+  req.session.destroy(error => {
+    if (error) {
+      return res.redirect("/home")
+    }
+    res.clearCookie(SESS_NAME);
+    res.redirect("/login");
+  })
 })
 
 export {loginauth}
