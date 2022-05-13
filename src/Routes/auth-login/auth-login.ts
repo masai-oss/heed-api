dea720d1bfaaa938a8121bd2f42440b5ef949111
Shivam Pandey <sap0442@gmail.com>
2022-05-13 14:05:46 +0530

    initial setup of authentication

--- src/Routes/auth-login/auth-login.ts
+++ src/Routes/auth-login/auth-login.ts
@@ -3,21 +3,23 @@ const session = require("express-session");
 // const app = express();
 const loginauth = Router();
 
-const mid = (req: { session: any; },res: any,next: any) => {
+const authenicate = (req: { session: any; },res: any,next: any) => {
   console.log(req.session)
   next()
 }
 
-loginauth.get("",mid,(req: any,res: { send: (arg0: string) => any; }) => {
+loginauth.get("",authenicate,(req: any,res: { send: (arg0: string) => any; }) => {
+    const {token} = req.session;
     return res.send(`
-      <h1>Welcome to heed!</h1>
-      <a href='/login'>Login</a>
-      <a href='/register'>Register</a>
+    <h1>Welcome to heed!</h1>
+    ${!token?`<a href='/login'>Login</a>
+    <a href='/register'>Register</a>`:`<a href='/home'>Home</a>
+    <form method="post" action="/logout">
+      <button>Logout</button>
+    </form>`}
+      
 
-      <a href='/home'>Home</a>
-      <form method="post" action="/logout">
-        <button>Logout</button>
-      </form>
+      
     `)
 })
 
@@ -46,11 +48,36 @@ loginauth.get("/register",() => {
 
 loginauth.post("/login",(req: { body: any; },res: any) => {
 
-  console.log(req.body)
-  return res.send({
-    status: 200,
-    message:"Login successful"
-  })
+  try {
+    // check the format of email first if not proper throw an error 
+
+    // else check wheather email is present
+    const user = await User.find({ email: req.body.email });
+
+    // if not present throw an error 
+    if (!user) return res.status(400).json({ status: "failed", message: "Provide a valid email address" })
+
+    // else verify the password
+    const validPassword = await bcrypt.compare(req.body.password, user.password);
+    if (validPassword) {
+      res.status(200).json({ message: "Valid password" });
+    } 
+    // if password not match throw an error
+    else {
+      res.status(400).json({ error: "Invalid Password" });
+    }
+    // else create a new token 
+    const token = newToken(user);
+
+    req.session.token = token;
+    // return the user and the token
+
+    return res.redirect("/home")
+
+} catch (e) {
+    return res.status(500).json({ status: "failed", message: e.message });
+}
+  
 
 })
 
